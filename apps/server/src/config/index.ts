22c758d313e294fe6ecfe0c159db3c6837c5bb17
Benzazoua <billalben252@gmail.com>
2026-05-08 20:29:01 +0100

    refactor: update WHITELIST_ADMINS_MAIL to use environment variable

--- apps/server/src/config/index.ts
+++ apps/server/src/config/index.ts
@@ -13,7 +13,10 @@ const config = {
     process.env.JWT_REFRESH_SECRET || "default_refresh_secret",
   ACCESS_TOKEN_EXPIRY: "1h" as const,
   REFRESH_TOKEN_EXPIRY: "7d" as const,
-  WHITELIST_ADMINS_MAIL: ["billal@domain.com", "admin@domain.com"],
+  // WHITELIST_ADMINS_MAIL: ["billal@domain.com", "admin@domain.com"],
+  WHITELIST_ADMINS_MAIL: process.env.WHITELIST_ADMINS_MAIL
+    ? process.env.WHITELIST_ADMINS_MAIL.split(",")
+    : [],
 };
 
 export default config;
