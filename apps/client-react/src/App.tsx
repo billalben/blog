a91c8a0245c9fac574fd31e428b849c2e502bea2
Benzazoua <billalben252@gmail.com>
2026-07-25 17:55:58 +0100

    feat: install packages

--- apps/client-react/src/App.tsx
+++ apps/client-react/src/App.tsx
@@ -1,5 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
 const App = () => {
-  return <div>App up</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      App up
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
